2d9b041e6eb8bcaf8f109b84f0d9053f9ca4b9d0
Nelu Platonov <neluplatonov@Nelus-MacBook-Pro.local>
2017-11-13 18:02:33 +0100

    Added google fonts: Roboto and Ubuntu, added background image + color overlay for header.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="area">
-
+      <div class="img">
+          <div class="colorOverlay">
+              <h1>XDDD</h1>
+              <p>AYYYY TEST</p>
+          </div>
+      </div>
   </div>
 </template>
 <script>
@@ -10,7 +15,26 @@
     .area{
         height: 100vh;
         width: 100vw;
-        background-color: green;
-
+       
+    }
+    .img{
+        height: 100vh;
+        width: 100vw;
+        background-image: url("../assets/background-code.jpg");
+        background-size: cover;
+        background-repeat: no-repeat
+    }
+    .colorOverlay{
+        width: 100vw;
+        height: 100vh;
+        background-color: rgba(100, 255, 100, 0.7);
+    }
+    h1{
+        font-family: 'Ubuntu', sans-serif;
+        margin: 0
+    }
+    p{
+        font-family: 'Roboto', sans-serif;
+        font-size: 30px
     }
 </style>
