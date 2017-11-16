f75194b6d8311a9268a929825de9fbc8b37d99de
Nelu Platonov <neluplatonov@Nelus-MacBook-Pro.local>
2017-11-16 23:00:13 +0100

    Started about Page, styling/positioning in CSS is so annoying. Flexbox isn't working like i thought it would, gonna try CSS grid tomorrow.

--- src/components/routes/About.vue
+++ src/components/routes/About.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-      <h2>ABOUT MY ASS</h2>
+      <h2>ABOUT ME</h2>
+      <div class="container">
+        <div class="aboutText">My name is Nelu Ion Platonov. <br> I'm a 20 year old self-taught web developer from Moldova who moved to Belgium in 2012.</div>
+        <img src="../../assets/me.png" alt="">
+      </div>
   </div>
 </template>
 <script>
@@ -8,6 +12,25 @@
 </script>
 <style scoped>
   h2{
+    font-family: 'Ubuntu', sans-serif;
+    color: #327F32;
     display: inline-block
+  }
+  .container{
+    align-items: center;
+    flex-direction: row;
+  }
+  img{
+    display: inline;
+    width: 20vw;
+    margin-left: 30vw;
+  }
+  .abouText{
+    display: absolute;
+    margin-top: 30vw;
+    margin-left: 25vw;
+  }
+  div{
+     font-family: 'Roboto', sans-serif;
   }
 </style>
