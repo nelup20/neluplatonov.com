2c14b6beda338bf4abed85bc277a4f352ce15451
Nelu Platonov <neluplatonov@Nelus-MacBook-Pro.local>
2017-11-16 15:12:29 +0100

    Finished Portfolio viewer, next need to finish the logic for freelancer jobs + style so that the router/main content height gets bigger. Also need to do animation of components/routes next.

--- src/components/routes/Portfolio.vue
+++ src/components/routes/Portfolio.vue
@@ -1,23 +1,54 @@
 <template>
   <div >
     <h2>PORTFOLIO</h2>
-      <div>
-        Site 1
+        <div class="portfolioNav">
+          <i class="fa fa-chevron-left" aria-hidden="true" id="leftArrow" @click="PortfolioSite(-1)"></i>
+        <div>
+          <a><img :src="`./src/assets/${previousSite.img}.png`" class="siteLogo otherSites" alt="" @click="PortfolioSite(-1)"></a>
+        </div>
+        <div>
+          <a :href="currentSite.link" target="_blank"><img :src="`./src/assets/${currentSite.img}.png`" class="siteLogo" alt=""></a>
+        </div>
+        <div>
+          <a><img :src="`./src/assets/${nextSite.img}.png`" class="siteLogo otherSites" alt="" @click="PortfolioSite(1)" ></a>
+        </div>
+        <i class="fa fa-chevron-right" aria-hidden="true" @click="PortfolioSite(1)"></i>
       </div>
       <div>
         <div class="techUsedBar" :style="expandBar"  @click="activate">
         <span id="barTitle">Technologies Used</span>
         <i class="fa fa-plus-circle" aria-hidden="true" :class="{ 'fa fa-minus-circle' : active }"></i>
        <div class="expandTech" :style="showTech">
-          <div>Front-end: <img src="../../assets/vueLogo.png" alt="" class="logo">, <img src="../../assets/htmlLogo.png" alt="" class="logo"> , Meme.js</div>
-           <div>Back-end: Haskell, C++, NickSQL</div>
-           <div>Development: Webpack, Babel</div>
+          <div>Front-end: <img v-for="(tech, index) in frontEnd" :key="index" :src="`./src/assets/${tech}.png`" class="logo" alt=""> </div>
+           <div>Back-end: <img v-for="(tech, index) in backEnd" :key="index" :src="`./src/assets/${tech}.png`" class="logo" alt=""> </div>
+           <div>Development: <img v-for="(tech, index) in development" :key="index" :src="`./src/assets/${tech}.png`" class="logo" alt=""> </div>
        </div>
         </div>
       </div>
       <h2>FREELANCE JOBS</h2>
+      <div class="portfolioNav">
+          <i class="fa fa-chevron-left" aria-hidden="true" id="leftArrow" @click="PortfolioSite(-1)"></i>
+        <div>
+          <a><img :src="`./src/assets/${previousSite.img}.png`" class="siteLogo otherSites" alt="" @click="PortfolioSite(-1)"></a>
+        </div>
+        <div>
+          <a :href="currentSite.link" target="_blank"><img :src="`./src/assets/${currentSite.img}.png`" class="siteLogo" alt=""></a>
+        </div>
+        <div>
+          <a><img :src="`./src/assets/${nextSite.img}.png`" class="siteLogo otherSites" alt="" @click="PortfolioSite(1)" ></a>
+        </div>
+        <i class="fa fa-chevron-right" aria-hidden="true" @click="PortfolioSite(1)"></i>
+      </div>
       <div>
-        Site 1
+        <div class="techUsedBar" :style="expandBar"  @click="activate">
+        <span id="barTitle">Technologies Used</span>
+        <i class="fa fa-plus-circle" aria-hidden="true" :class="{ 'fa fa-minus-circle' : active }"></i>
+       <div class="expandTech" :style="showTech">
+          <div>Front-end: <img v-for="(tech, index) in frontEnd" :key="index" :src="`./src/assets/${tech}.png`" class="logo" alt=""> </div>
+           <div>Back-end: <img v-for="(tech, index) in backEnd" :key="index" :src="`./src/assets/${tech}.png`" class="logo" alt=""> </div>
+           <div>Development: <img v-for="(tech, index) in development" :key="index" :src="`./src/assets/${tech}.png`" class="logo" alt=""> </div>
+       </div>
+        </div>
       </div>
   </div>
 </template>
@@ -37,29 +68,58 @@
             display: "none",
             transition: 'display 0.2s'
           },
-          usedTech: {
-            frontEnd: {
-              html: true,
-              css: true,
-              javascript: true,
-              vue: true,
-            },
-            backEnd: {
+          highlightedSiteIndex: 0,
+          frontEnd: '',
+          backEnd: '',
+          development: '',
+          currentSite: '',
+          previousSite: '',
+          nextSite: ''
 
-            },
-            development: {
-
-            }
-          }
         }
       },
       methods: {
         activate(){
           this.active = !this.active;
-          this.active ? this.expandBar.height = "10.5vw" : this.expandBar.height = "1.6vw";
-          this.active ? this.showTech.display = "inline" : this.showTech.display = "none"
+          this.active ? this.expandBar.height = "13vw" : this.expandBar.height = "1.6vw";
+          this.active ? this.showTech.display = "inline" : this.showTech.display = "none";
+        },
+        PortfolioSite(amount){
+          if(amount === -1 && this.highlightedSiteIndex === 0){
+            this.highlightedSiteIndex = 16
+          } else if(amount === 1 && this.highlightedSiteIndex === 16){
+            this.highlightedSiteIndex = 0
+          } else {
+            this.highlightedSiteIndex += amount;
+          };
+          this.frontEnd = this.$store.state.sites[this.highlightedSiteIndex].frontEnd;
+          this.backEnd = this.$store.state.sites[this.highlightedSiteIndex].backEnd;
+          this.development = this.$store.state.sites[this.highlightedSiteIndex].development;
+          this.currentSite = this.$store.state.sites[this.highlightedSiteIndex];
+          if(this.highlightedSiteIndex === 16){
+            this.nextSite = this.$store.state.sites[0]
+          } else {
+            this.nextSite = this.$store.state.sites[this.highlightedSiteIndex + 1];
+          }
+          if(this.highlightedSiteIndex === 0){
+            this.previousSite = this.$store.state.sites[16];
+          } else {
+          this.previousSite = this.$store.state.sites[this.highlightedSiteIndex - 1];
+          }
         }
-      }
+      },
+      created(){
+          this.frontEnd = this.$store.state.sites[this.highlightedSiteIndex].frontEnd;
+          this.backEnd = this.$store.state.sites[this.highlightedSiteIndex].backEnd;
+          this.development = this.$store.state.sites[this.highlightedSiteIndex].development;
+          this.currentSite = this.$store.state.sites[this.highlightedSiteIndex];
+          this.nextSite = this.$store.state.sites[this.highlightedSiteIndex + 1];
+          if(this.highlightedSiteIndex === 0){
+            this.previousSite = this.$store.state.sites[17];
+          } else {
+          this.previousSite = this.$store.state.sites[this.highlightedSiteIndex - 1];
+          }
+      },
     }
 </script>
 
@@ -101,10 +161,48 @@
     margin-top: 1.2vw;
   }
   .logo{
-    height: 3vh;
+    height: 4.3vh;
+    margin-left: 1vw;
+  }
+  .portfolioNav{
+    display: flex;
+  }
+  .portfolioNav div{
+    flex: 1;
+  }
+  .siteLogo{
+    height: 18vh;
+    width: 11vw ;
+    border-radius: 0%;
+    box-shadow: 1px 1px 7px black;
+    margin-bottom: 3vh;
+    transition: all 0.5s;
+  }
+  .siteLogo:hover{
+    border-radius: 50%;
+  }
+  .portfolioNav i{
+    margin-top: 6.5vh;
+    margin-left: -5vw;
+    margin-right: 5vw;
+    font-size: 3vw;
+    color: darkorange;
+  }
+  .portfolioNav i:hover{
+    cursor: pointer;
+  }
+  .otherSites{
+    height: 9vh;
+    width: 5.5vw;
+    margin-top: 4vh;
+    cursor: pointer;
+  }
+  #leftArrow{
+    margin-top: 6.5vh;
+    margin-left: 5vw;
+    margin-right: -5vw;
   }
 
-  
 
 </style>
 <!--  height of bar when expanded height: 11vw; expandtech height: 10vw -->
---
+++ src/store/store.js
@@ -0,0 +1,155 @@
+import Vue from "vue"
+import Vuex from 'vuex'
+
+Vue.use(Vuex);
+
+export const store = new Vuex.Store({
+    state: {
+        sites: [
+            {
+                name: 'neluplatonov.com',
+                frontEnd: ['html','css','javascript','vue'],
+                backEnd: ['node','heroku'],
+                development: ['git','npm','babel','webpack'],
+                img: 'testSite1',
+                link: 'https://www.google.com'
+            },
+            {
+                name: 'meme.com',
+                frontEnd: ['css','vue'],
+                backEnd: ['heroku'],
+                development: ['git','webpack'],
+                img: 'testSite2',
+                link: 'https://www.facebook.com'
+            },
+            {
+                name: 'meme.com',
+                frontEnd: ['css','vue'],
+                backEnd: ['heroku'],
+                development: ['git','webpack'],
+                img: 'testSite2',
+                link: 'https://www.facebook.com'
+            },
+            {
+                name: 'meme.com',
+                frontEnd: ['css','vue'],
+                backEnd: ['heroku'],
+                development: ['git','webpack'],
+                img: 'testSite2',
+                link: 'https://www.facebook.com'
+            },
+            {
+                name: 'meme.com',
+                frontEnd: ['css','vue'],
+                backEnd: ['heroku'],
+                development: ['git','webpack'],
+                img: 'testSite2',
+                link: 'https://www.facebook.com'
+            },
+            {
+                name: 'meme.com',
+                frontEnd: ['css','vue'],
+                backEnd: ['heroku'],
+                development: ['git','webpack'],
+                img: 'testSite2',
+                link: 'https://www.facebook.com'
+            },
+            {
+                name: 'meme.com',
+                frontEnd: ['css','vue'],
+                backEnd: ['heroku'],
+                development: ['git','webpack'],
+                img: 'testSite2',
+                link: 'https://www.facebook.com'
+            },
+            {
+                name: 'meme.com',
+                frontEnd: ['css','vue'],
+                backEnd: ['heroku'],
+                development: ['git','webpack'],
+                img: 'testSite2',
+                link: 'https://www.facebook.com'
+            },
+            {
+                name: 'meme.com',
+                frontEnd: ['css','vue'],
+                backEnd: ['heroku'],
+                development: ['git','webpack'],
+                img: 'testSite2',
+                link: 'https://www.facebook.com'
+            },
+            {
+                name: 'meme.com',
+                frontEnd: ['css','vue'],
+                backEnd: ['heroku'],
+                development: ['git','webpack'],
+                img: 'testSite2',
+                link: 'https://www.facebook.com'
+            },
+            {
+                name: 'meme.com',
+                frontEnd: ['css','vue'],
+                backEnd: ['heroku'],
+                development: ['git','webpack'],
+                img: 'testSite2',
+                link: 'https://www.facebook.com'
+            },
+            {
+                name: 'meme.com',
+                frontEnd: ['css','vue'],
+                backEnd: ['heroku'],
+                development: ['git','webpack'],
+                img: 'testSite2',
+                link: 'https://www.facebook.com'
+            },
+            {
+                name: 'meme.com',
+                frontEnd: ['css','vue'],
+                backEnd: ['heroku'],
+                development: ['git','webpack'],
+                img: 'testSite2',
+                link: 'https://www.facebook.com'
+            },
+            {
+                name: 'meme.com',
+                frontEnd: ['css','vue'],
+                backEnd: ['heroku'],
+                development: ['git','webpack'],
+                img: 'testSite2',
+                link: 'https://www.facebook.com'
+            },
+            {
+                name: 'meme.com',
+                frontEnd: ['css','vue'],
+                backEnd: ['heroku'],
+                development: ['git','webpack'],
+                img: 'testSite2',
+                link: 'https://www.facebook.com'
+            },
+            {
+                name: 'meme.com',
+                frontEnd: ['css','vue'],
+                backEnd: ['heroku'],
+                development: ['git','webpack'],
+                img: 'testSite2',
+                link: 'https://www.facebook.com'
+            },
+            {
+                name: 'meme.com',
+                frontEnd: ['css','vue'],
+                backEnd: ['heroku'],
+                development: ['git','webpack'],
+                img: 'testSite2',
+                link: 'https://www.facebook.com'
+            },
+            {
+                name: 'meme.com',
+                frontEnd: ['css','vue'],
+                backEnd: ['heroku'],
+                development: ['git','webpack'],
+                img: 'testSite2',
+                link: 'https://www.facebook.com'
+            },
+        ]
+    }
+});
